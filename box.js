bccac8e94d2d3bfd5e7cd1518cc26ff32ed24374
asuna-cross <littlecrystaldragon@gmail.com>
2023-02-02 13:53:28 +0000

    Working grid code

--- box.js
+++ box.js
@@ -1,11 +1,17 @@
-var n=4;//take grid column value as you want
+var columns = 4;
+var rows = 4;
 
-for(var i = 0; i < n; i++) {
-    document.body.innerHTML+='<div class="row">';
-
-        for(j = 0; j < n; j++) {
-            document.body.innerHTML+='<div class="gridsquare">' + (i*5+j+1) + '</div>';
-        }
-
-    document.body.innerHTML+='</div>';
+var grid = document.createElement('div');
+grid.className = 'grid';
+for (var i = 0; i < columns; ++i) {
+    var column = document.createElement('div'); // create column
+    column.className = 'column';
+    for (var j = 0; j < rows; ++j) {
+        var row = document.createElement('div'); // create row
+        row.className = 'row';
+        row.textContent = i + '-' +j; // set text
+        column.appendChild(row); // append row in column
+    }
+    grid.appendChild(column); // append column inside grid
 }
+document.body.appendChild(grid);
